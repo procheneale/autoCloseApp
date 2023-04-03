e003511b60d444e5ae30311f94a70eb5c5573ebc
Paul Pedrozo <procheneale@gmail.com>
2023-04-03 16:04:14 -0400

    added a timer and input validation so users can only put integers

--- app.py
+++ app.py
@@ -4,9 +4,26 @@ import keyboard
 
 def main():
     print("How many hours until shut-down?")
-    usrTime = int(input("Time (Hour): ")) * 3600
+    usrInput = ""
+    while True:
+        usrInput = input("Time (Hour): ")
+        if not usrInput.isdigit():
+            print("Sorry, please enter a valid integer.")
+            continue
+        else:
+            break
+
+    usrTime = int(usrInput) * 3600
     
-    time.sleep(usrTime)
+    for i in range(usrTime):
+        remaining_seconds = usrTime - i
+        hours = remaining_seconds // 3600
+        minutes = (remaining_seconds % 3600) // 60
+        seconds = remaining_seconds % 60
+        print(f"Remaining time: {hours:02d}:{minutes:02d}:{seconds:02d}", end="\r")
+        time.sleep(1)
+    
+    print()
     
     keyboard.press('cmd+q')
     time.sleep(1)
